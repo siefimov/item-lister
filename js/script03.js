ee4808f5982efdfdcec214c3034f4f8cb78bce01
serhii yefimov <sergii.iefimov@gmail.com>
2022-04-29 16:47:22 +0300

    ended with part 3

--- js/script03.js
+++ js/script03.js
@@ -15,12 +15,40 @@ console.log(itemList.parentElement.parentElement)
 // console.log(itemList.childNodes);
 
 // children
-console.log(itemList.children);
-console.log(itemList.children[0]);
+console.log(itemList.children)
+console.log(itemList.children[0])
 itemList.children[1].style.backgroundColor = 'lightgreen'
 
 // firstChild - НУ ДУЖЕ КОРИСНА
-console.log(itemList.firstChild);
+console.log(itemList.firstChild)
 
 // firstElementChild
-console.log(itemList.firstElementChild);
+console.log(itemList.firstElementChild)
+
+// nextSibling
+console.log(itemList.nextSibling)
+
+// nextElementSibling
+console.log(itemList.nextElementSibling)
+
+// createElement
+const newDiv = document.createElement('div')
+// add class
+newDiv.className = 'hello'
+// add id
+newDiv.id = 'hello1'
+
+newDiv.setAttribute('title', 'Hello Div')
+
+// create text node
+const newDivText = document.createTextNode('Hello world!')
+
+// add text to div
+newDiv.appendChild(newDivText)
+
+const container = document.querySelector('div .card')
+const h1 = document.querySelector('div h2')
+
+container.insertBefore(newDiv, h1)
+
+console.log(newDiv)
